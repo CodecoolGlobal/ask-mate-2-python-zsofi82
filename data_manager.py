7c23a9b084928716d2c08daf0f3121cfb2bb8235
zsofi82 <zsofigaal@gmail.com>
2022-02-15 12:12:14 +0100

    Add list route

---
+++ data_manager.py
@@ -0,0 +1,8 @@
+import os
+
+
+quesiton_file_path = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
+QUESTION_HEADER = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+question_headers = ["Id", "Submission time", "View number", "Vote number", "Title", "Message", "Image"]
+ANSWER_HEADER = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+answer_headers = ["Id", "Submission time", "Vote number", "Question ID", "Message", "Image"]
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+import data_manager
+import connection
 
 app = Flask(__name__)
 
@@ -6,6 +8,43 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/list")
+def display_questions():
+    data_file = data_manager.quesiton_file_path
+    questions = connection.get_data_from_csv(data_file)
+    return render_template("list_questions.html", questions=questions, headers=data_manager.question_headers)
+
+
+# @app.route("/question/<question_id>")
+# def display_a_question():
+#     pass
+#
+#
+# @app.route("/add-question")
+# def add_a_question():
+#     pass
+#
+#
+# @app.route("/question/<question_id>/new-answer")
+# def post_an_answer():
+#     pass
+#
+#
+# @app.route("/question/<question_id>/delete")
+# def post_an_answer():
+#     pass
+#
+#
+# @app.route("/question/<question_id>/edit")
+# def post_an_answer():
+#     pass
+#
+#
+# @app.route("/answer/<answer_id>/delete")
+# def post_an_answer():
+#     pass
 
 
 if __name__ == "__main__":
